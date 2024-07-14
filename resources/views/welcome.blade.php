b73687b103dd5f7ccbd662b4f3feb315da276eb4
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-14 13:46:54 +0600

    design correction

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,9 +17,8 @@
 @section('body')
 
 
-<div class="bg-cover-custom">
-    <div class="backdrop-blur flex items-center justify-center min-h-screen">
-    <div class="">
+<div class="bg-cover-custom ">
+    <div class="backdrop-blur flex justify-center items-start min-h-screen pt-10">
         <div class="bg-white p-5 shadow-md max-w-md w-full relative z-10 pt-8">
             <h1 class="text-4xl font-bold mb-4">Enroll in your complimentary ID Theft Protection</h1>
             <p class=" mb-4">
@@ -29,16 +28,15 @@
             <form>
                 <label for="card-number" class="block text-gray-700 mb-2">16-Digit Card Number</label>
                 <input type="text" id="card-number" name="card-number" maxlength="16" class="border border-red-500 p-2 w-full rounded mb-4" placeholder="****************">
-                <button type="submit" class="bg-red-500 text-white py-1 px-4 rounded w-full mb-4">Verify</button>
+                <button type="submit" class="bg-red-500 text-white py-1 px-4 rounded w-full mb-3">Verify</button>
             </form>
-            <div class="text-center">
-                <p class="text-gray-700">Already have an account?</p>
+            <div class="">
+                <p class="text-gray-700 pb-2">Already have an account?</p>
                 <div class="text-center border-2 border-red-300 rounded w-full">
                     <button class="font-semibold mt-1 mb-1 text-red-300">Log In</button>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 </div>
 @endsection
