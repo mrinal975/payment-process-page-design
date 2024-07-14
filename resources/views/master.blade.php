3c70f34cef0433ca7604e6a39d77feb0f5f549bf
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-07-14 11:29:49 +0600

    init commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Google Tag Manager -->
+
+    <!-- End Google Tag Manager -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- SEO Meta Tags -->
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+
+
+    <meta name="facebook-domain-verification" content="mnboul782i63d9fijs7oh0e185reyh"/>
+
+
+    <link rel="icon" href="{{ asset('assets/img/Quizee_Favicon.png') }}" sizes="32x32" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" defer>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <title>Master-card</title>
+
+    <!-- Meta Pixel Code -->
+
+    <!-- End Meta Pixel Code -->
+
+    @yield('page-specific-css')
+</head>
+
+<body>
+<!-- Google Tag Manager (noscript) -->
+<!-- End Google Tag Manager (noscript) -->
+
+
+@yield('body')
+
+@yield('page-specific-js')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('master')
+@section('page-specific-css')
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+@endsection
+
+@section('body')
+    <div class="">
+        <p class="">baba loknath</p>
+    </div>
+@endsection
+
+@section('page-specific-js')
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+@endsection
+
+
+@section('page-specific-css')
+    <style>
+
+    </style>
+@endsection
